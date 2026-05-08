b9262018f27722777f52ff0a6145f6bc45728454
shalabyelectronics <mhdshalabystar@gmail.com>
2026-05-08 23:32:41 +0300

    Add array methods exercises: map, filter, reduce

--- week1-day1.js
+++ week1-day1.js
@@ -1,7 +1,8 @@
 "module";
+//Add array methods exercises: map, filter, reduce
 const prices = [100, 250, 80, 420, 55];
 // Expected result: [115, 287.5, 92, 483, 63.25]
-const test = prices.map((number) => number + ((number * 15)/100));
+const test = prices.map((number) => number + (number * 15) / 100);
 console.log(test);
 
 const students = [
