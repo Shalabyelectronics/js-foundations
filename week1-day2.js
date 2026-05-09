1acf14535df27fc7d9e7b89d9cb06000e7202ee0
shalabyelectronics <mhdshalabystar@gmail.com>
2026-05-09 19:46:09 +0300

    Add week1-day2 exercises: demonstrate variable scope and closures

--- week1-day2.js
+++ week1-day2.js
@@ -1,1 +1,49 @@
 console.log("Week1-day2")
+const city = "Cairo";
+
+function printCity() {
+  const city = "Alexandria";
+  console.log(city);
+}
+
+printCity();
+console.log(city);
+
+// ----------------
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log("var:", i), 100);
+}
+// 3, 3 , 3
+
+for (let j = 0; j < 3; j++) {
+  setTimeout(() => console.log("let:", j), 100);
+}
+// 0,1,2
+
+//----------------
+
+const makeGreeting = lang => {
+    return function(name){
+        if (lang === "arabic"){
+            console.log(`أهلاً ${name}`)
+        }else if(lang === "english"){
+            console.log(`Hello ${name}`)
+        }else{
+            console.log(`Hello ${name} anyway we are working on this new language😊`)
+        }
+
+    }
+}
+
+const greetInArabic = makeGreeting("arabic");
+const greetInEnglish = makeGreeting("english");
+
+
+greetInArabic("Mohamed"); // "أهلاً Mohamed"
+greetInEnglish("Mohamed"); // "Hello Mohamed"
+
+//-------------
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 100);
+}
